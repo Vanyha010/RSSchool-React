1325fd17d621338830d0bb561106b5d97b1b9242
Vanyha010 <vanyha010@icloud.com>
2024-07-08 19:40:03 +0300

    fix: change project setup so react-compiler would work

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,40 @@
-import { Component } from 'react';
-import PokemonSearch from './components/PokemonSearch/PokemonSearch';
-import ErrorBoundary from './components/ErrorBoundary/ErrorBoundary';
+// import { Component } from 'react';
+// import PokemonSearch from './components/PokemonSearch/PokemonSearch';
+// import ErrorBoundary from './components/ErrorBoundary/ErrorBoundary';
 
-export default class App extends Component {
-    render() {
-        return (
-            <main>
-                <ErrorBoundary>
-                    <PokemonSearch />
-                </ErrorBoundary>
-            </main>
-        );
-    }
+// export default class App extends Component {
+//     render() {
+//         return (
+//             <main>
+//                 <ErrorBoundary>
+//                     <PokemonSearch />
+//                 </ErrorBoundary>
+//             </main>
+//         );
+//     }
+// }
+
+import { useState } from 'react';
+
+function Header() {
+    console.log('Header', Math.random());
+    return (
+        <header>
+            <h1>React Counter</h1>
+        </header>
+    );
+}
+
+export default function App() {
+    const [count, setCount] = useState(0);
+
+    return (
+        <>
+            <Header />
+            <div>
+                <p>{count}</p>
+                <button onClick={() => setCount(count + 1)}>Increment</button>
+            </div>
+        </>
+    );
 }
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,18 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
-export default defineConfig({
-    plugins: [react()],
+const ReactCompilerConfig = {};
+
+export default defineConfig(() => {
+  return {
+    plugins: [
+      react({
+        babel: {
+          plugins: [
+            ["babel-plugin-react-compiler", ReactCompilerConfig],
+          ],
+        },
+      }),
+    ],
+  };
 });

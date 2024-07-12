8953cb95552b91f653873944b24813290ba3173a
Vanyha010 <vanyha010@icloud.com>
2024-07-12 18:14:52 +0300

    feat: convert App component to functional

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,12 @@
-import { Component } from 'react';
 import PokemonSearch from './components/PokemonSearch/PokemonSearch';
 import ErrorBoundary from './components/ErrorBoundary/ErrorBoundary';
 
-export default class App extends Component {
-    render() {
-        return (
-            <main>
-                <ErrorBoundary>
-                    <PokemonSearch />
-                </ErrorBoundary>
-            </main>
-        );
-    }
+export default function App() {
+    return (
+        <main>
+            <ErrorBoundary>
+                <PokemonSearch />
+            </ErrorBoundary>
+        </main>
+    );
 }

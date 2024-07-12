98af3041ac943e78efc2d1b04df2233ef0200302
Vanyha010 <vanyha010@icloud.com>
2024-07-12 19:28:15 +0300

    feat: convert PokemonSearch component to functional

--- src/components/PokemonSearch/PokemonSearch.tsx
+++ src/components/PokemonSearch/PokemonSearch.tsx
@@ -1,41 +1,19 @@
-import React from 'react';
+import { useState } from 'react';
 import SearchBar from '../SearchBar/SearchBar';
 import PokemonsList from '../PokemonsList/PokemonsList';
 import { PokemonCardData } from '../../types/types';
 import Loader from '../Loader/Loader';
 
-type PokemonSearchState = {
-    pokemons: PokemonCardData[] | undefined[];
-    isLoading: boolean;
-};
+export default function PokemonSearch() {
+    const [pokemons, setPokemons] = useState<(PokemonCardData | undefined)[]>(
+        [],
+    );
+    const [isLoading, setIsLoading] = useState(false);
 
-export default class PokemonSearch extends React.Component {
-    public state: PokemonSearchState = {
-        pokemons: [],
-        isLoading: false,
-    };
-
-    setPokemons = (pokemonsData: (PokemonCardData | undefined)[]) => {
-        this.setState({ pokemons: pokemonsData });
-    };
-
-    setLoading = (loadingStatus: boolean) => {
-        this.setState({ isLoading: loadingStatus });
-    };
-
-    render(): React.ReactNode {
-        return (
-            <>
-                <SearchBar
-                    setPokemons={this.setPokemons}
-                    setLoading={this.setLoading}
-                />
-                {this.state.isLoading ? (
-                    <Loader />
-                ) : (
-                    <PokemonsList pokemons={this.state.pokemons} />
-                )}
-            </>
-        );
-    }
+    return (
+        <>
+            <SearchBar setPokemons={setPokemons} setLoading={setIsLoading} />
+            {isLoading ? <Loader /> : <PokemonsList pokemons={pokemons} />}
+        </>
+    );
 }
